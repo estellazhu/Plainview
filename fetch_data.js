c7c27823176a2fa44dfa862dc2be19d8d8588d44
Estella <hongzi.emma@gmail.com>
2017-03-01 19:41:41 -0500

    Get JSON data from external JSON files

--- fetch_data.js
+++ fetch_data.js
@@ -1,38 +1,30 @@
 var noodle = require('noodlejs');
+var fs = require('fs');
 
-var domainTags = {
-	"abcnews": {
-		'tagsRetrieval': [
-			{"tag" : "p", "fieldName": "content"},
-			{"tag" : "div.author", "fieldName": "author"},
-			{"tag" : "span.timestamp", "fieldName": "date"}
- 		],
-		'url' : 'http://www.abcnews.com'
-	},
-	"bbc" : {
-		'tagsRetrieval': [
-			{"tag" : "p", "fieldName": "content"},
-			{"tag" : "div.byline", "fieldName": "author"},
-			{"tag" : "div.date--v2", "fieldName": "date"}
-		]
-	}
-}
 
 // Testing
-var url = "http://abcnews.go.com/International/wireStory/official-mexico-rejected-us-plan-3rd-country-deportees-45712867?cid=clicksource_4380645_1_hero_headlines_headlines_hed";
+var url = "http://www.bbc.com/sport/football/39024318";
 var domain = getDomain(url);
+var getJson = "json/" + domain + ".json";
 var content = {};
 var jsonToReturn = {};
+var domainTags;
 
-getData(url, domainTags[domain].tagsRetrieval, 0)
-.then(function(){
-	for (var property in content) {
-    if (content.hasOwnProperty(property)) {
-        jsonToReturn[property] = content[property][0].results;
-    }
-	}
-	console.log(jsonToReturn);
+fs.readFile(getJson, 'utf8', function (err, data) {
+  if (err) throw err;
+  domainTags = JSON.parse(data);
+	getData(url, domainTags[domain].tagsRetrieval, 0)
+	.then(function(){
+		for (var property in content) {
+	    if (content.hasOwnProperty(property)) {
+	        jsonToReturn[property] = content[property][0].results;
+	    }
+		}
+		console.log(jsonToReturn);
+	});
 });
+
+
 
 // Get data from website html
 function getData(url, tagInfo, index){
@@ -54,9 +46,9 @@ function getData(url, tagInfo, index){
 				}
 			}).catch(function(err){
 				getData(url, tagInfo, index+1)
-						.then(function(){
-							resolve();
-						});
+					.then(function(){
+						resolve();
+					});
 			});
 		});
 }
@@ -73,3 +65,4 @@ function getDomain(url) {
 	endOfDomain = url.indexOf(".", startOfDomain);
 	return url.substring(startOfDomain, endOfDomain);
 }
+
